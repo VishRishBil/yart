f3b72e0e5b5999dfacaa0039f668155b30ff67d9
bilinbs <bilinbs@gmail.com>
2016-01-09 07:12:58 +0530

    Adding User Bean

---
+++ src/main/java/com/yart/bean/User.java
@@ -0,0 +1,24 @@
+package com.yart.bean;
+
+import java.util.List;
+
+public class User {
+	private String userId;
+
+	private String email;
+
+	private String firstName;
+
+	private String password;
+
+	private byte userStatus;
+
+	private List<Contact> contacts;
+
+	private Role role;
+
+	private String locale;
+
+	private Project project;
+
+}
