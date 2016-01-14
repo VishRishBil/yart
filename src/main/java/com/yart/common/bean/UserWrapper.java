75c3c1459e4eff7f1bb24308ea6044669d00ebed
bilinbs <bilinbs@gmail.com>
2016-01-14 18:03:14 +0530

    Wrapper class for user bean

---
+++ src/main/java/com/yart/common/bean/UserWrapper.java
@@ -0,0 +1,38 @@
+package com.yart.common.bean;
+
+import com.yart.user.bean.User;
+
+/**
+ * Wrapper class for holding user bean and various workflow statuses
+ * 
+ * @author bilinbs
+ *
+ */
+public class UserWrapper {
+
+    private User user;
+
+    private STATUS_CODES statusCode;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public STATUS_CODES getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(STATUS_CODES statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    enum STATUS_CODES {
+        CREATE_OK, MODIFY_OK, USERID_EXISTS, EMAIL_EXISTS, VALID_CREDENTIALS, 
+        INVALID_CREDENTIALS, USER_INACTIVE
+    }
+
+}
